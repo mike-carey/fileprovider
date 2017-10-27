a2e83da481ea06ecc9a688b50fabdbc9ca949b9a
Mike Carey <carey012@rangers.uwp.edu>
2017-10-26 19:27:04 -0500

    Add mock test

---
+++ tests/providers/Mock.test.js
@@ -0,0 +1,40 @@
+/**
+ * @test providers.Mock
+ * @description Tests the providers.Mock module
+ */
+
+'use strict'
+
+const assert = require('chai').assert
+
+const support = require('../support')
+
+describe('providers.Mock', () => {
+
+    let Mock = undefined
+
+    beforeEach(() => {
+        Mock = support.require('../../src/providers/Mock')
+    })
+
+    it ('read', () => {
+        assert.isNotTrue((new Mock()).read())
+    })
+
+    it ('write', () => {
+        assert.isNotTrue((new Mock()).write())
+    })
+
+    it ('append', () => {
+        assert.isNotTrue((new Mock()).append())
+    })
+
+    it ('touch', () => {
+        assert.isNotTrue((new Mock()).touch())
+    })
+
+    it ('delete', () => {
+        assert.isNotTrue((new Mock()).delete())
+    })
+
+})
